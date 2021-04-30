50397e6840fa86234dd0669174fb5a259e4b56f9
Shin Yamagami <kiboh.usa@gmail.com>
2021-04-30 16:26:21 -0700

    done with the schedule class

--- src/pm_tool/pm_tool/Schedule.cs
+++ src/pm_tool/pm_tool/Schedule.cs
@@ -8,5 +8,35 @@ namespace pm_tool
 {
     class Schedule
     {
+        DateTime date;
+        Task taskName;
+
+
+        void setDate(DateTime newDate)
+        {
+            date = newDate;
+        }
+
+
+        Task getTask()
+        {
+            return taskName;
+        }
+
+        /*
+        // Wondering how the schedule class ended up having a getMember function.
+        // The project class is the one to have it, isn't it?
+        TeamMember getMember()
+        {
+
+        }*/
+
+
+        // Does update mean updating the Date?
+        void updateDate(DateTime newDate)
+        {
+            date = newDate;
+
+        }
     }
 }
